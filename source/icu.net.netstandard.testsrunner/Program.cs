4046e9dd2c40a7ae2b108ca1f308647e9aa47aa2
Connie Yau <conniey@microsoft.com>
2017-02-15 12:43:13 -0800

    Add icu.netstandard.testrunner

---
+++ source/icu.net.netstandard.testsrunner/Program.cs
@@ -0,0 +1,20 @@
+﻿using Icu.Tests;
+using NUnit.Common;
+using NUnitLite;
+using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace icu.net.netstandard.testsrunner
+{
+	public class Program
+	{
+		public static int Main(string[] args)
+		{
+			var result = new AutoRun(typeof(IcuWrapperTests).GetTypeInfo().Assembly)
+				.Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+			return result;
+		}
+	}
+}
