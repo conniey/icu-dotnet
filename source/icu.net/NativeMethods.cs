cbf960f71b5c75c2aa19d158c45ac0bc9be57073
Connie Yau <conniey@microsoft.com>
2017-02-28 14:54:32 -0800

    (MAY NEED FIXUP) Use LoadLibraryEx instead of LoadLibrary

--- source/icu.net/NativeMethods.cs
+++ source/icu.net/NativeMethods.cs
@@ -63,13 +63,29 @@ namespace Icu
 		#region Native methods for Windows
 
 		[DllImport("kernel32.dll", SetLastError = true)]
-		private static extern IntPtr LoadLibrary(string dllToLoad);
+		private static extern IntPtr LoadLibraryEx(string dllToLoad, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
 
 		[DllImport("kernel32.dll", SetLastError = true)]
 		private static extern bool FreeLibrary(IntPtr hModule);
 
 		[DllImport("kernel32.dll", SetLastError = true)]
 		private static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+		[Flags]
+		internal enum LoadLibraryFlags : uint
+		{
+			DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
+			LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010,
+			LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
+			LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040,
+			LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020,
+			LOAD_LIBRARY_SEARCH_APPLICATION_DIR = 0x00000200,
+			LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000,
+			LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x00000100,
+			LOAD_LIBRARY_SEARCH_SYSTEM32 = 0x00000800,
+			LOAD_LIBRARY_SEARCH_USER_DIRS = 0x00000400,
+			LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008
+		}
 
 		#endregion
 
@@ -155,11 +171,10 @@ namespace Icu
 			{
 				var libName = string.Format("{0}{1}.dll", basename, icuVersion);
 				var libPath = string.IsNullOrEmpty(_IcuPath) ? libName : Path.Combine(_IcuPath, libName);
-				var testPath = @"D:\git\conniey\sillsdev\icu-dotnet3\source\icu.net.netstandard.testsrunner\bin\Release\netcoreapp1.0\icuuc56.dll";
-
-				handle = LoadLibrary(libPath);
+				var testPath = @"D:\git\conniey\sillsdev\icu-dotnet3\source\icu.net.netstandard.testsrunner\bin\Debug\netcoreapp1.0\lib\x64\icuuc56.dll";
+				handle = LoadLibraryEx(libPath, IntPtr.Zero, LoadLibraryFlags.LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR | LoadLibraryFlags.LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
 				var lastError = Marshal.GetLastWin32Error();
-				var handle2 = LoadLibrary(testPath);
+				var handle2 = LoadLibraryEx(testPath, IntPtr.Zero, LoadLibraryFlags.LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR | LoadLibraryFlags.LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
 				var lastError2 = Marshal.GetLastWin32Error();
 				if (handle == IntPtr.Zero && lastError != 0)
 				{
